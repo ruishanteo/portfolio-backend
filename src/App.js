7b2dc51ed5cc69501411bf8964e1a7c544057418
ruishanteo <ruishan.teo03@gmail.com>
2023-06-22 10:11:17 +0800

    added routes and reducers

--- src/App.js
+++ src/App.js
@@ -6,34 +6,49 @@ import { auth } from "./backend/Firebase.js";
 
 import { Header } from "./components/Header.js";
 import { Home } from "./pages/Home.js";
-import { EditProject } from "./components/EditProject.js";
-import { NewProject } from "./components/NewProject.js";
+
+import { Projects } from "./pages/Projects.js";
+import { EditProject } from "./components/projectComponents/EditProject.js";
+import { NewProject } from "./components/projectComponents/NewProject.js";
+
+import { Experiences } from "./pages/Experiences.js";
+import { EditExperience } from "./components/experienceComponents/EditExperience.js";
+import { NewExperience } from "./components/experienceComponents/NewExperience.js";
+
+import { TechStack } from "./pages/TechStack.js";
+import { NewTech } from "./components/techStackComponents/NewTech.js";
+import { EditTech } from "./components/techStackComponents/EditTech.js";
 
 import Login from "./pages/Login";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { configureStore } from "@reduxjs/toolkit";
+import { experiencesReducer } from "./backend/experiencesStore.js";
 import { projectsReducer } from "./backend/projectsStore.js";
+import { techStackReducer } from "./backend/techStackStore.js";
 
 function App() {
   const [user, loading] = useAuthState(auth);
 
-  const store = configureStore({ reducer: projectsReducer });
+  const store = configureStore({
+    reducer: {
+      experiences: experiencesReducer,
+      projects: projectsReducer,
+      techstack: techStackReducer,
+    },
+  });
   let theme = createTheme({
     typography: {
-      fontFamily: ["Roboto", "Cambria"].join(","),
-    },
-    status: {
-      danger: "#ebaaa7",
+      fontFamily: ["Roboto"].join(","),
     },
     palette: {
       primary: {
-        main: "#576b86",
-        darker: "#053e85",
+        main: "#cbe0fb",
+        dark: "#053e85",
       },
-      neutral: {
-        main: "#576b86",
-        contrastText: "#fff",
+      secondary: {
+        main: "#968a79",
+        dark: "#fbe6cb",
       },
     },
   });
@@ -53,19 +68,50 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/new" element={<NewProject />} />
+
                     <Route
-                      path="/project/:projectId"
+                      exact
+                      path="/experiences/new"
+                      element={<NewExperience />}
+                    />
+                    <Route
+                      exact
+                      path="/experiences/:experienceId"
+                      element={<EditExperience />}
+                    />
+                    <Route
+                      exact
+                      path="/experiences"
+                      element={<Experiences />}
+                    />
+
+                    <Route
+                      exact
+                      path="/projects/new"
+                      element={<NewProject />}
+                    />
+                    <Route
+                      exact
+                      path="/projects/:projectId"
                       element={<EditProject />}
                     />
+                    <Route exact path="/projects" element={<Projects />} />
+
+                    <Route exact path="/techstack/new" element={<NewTech />} />
+                    <Route
+                      exact
+                      path="/techstack/:techId"
+                      element={<EditTech />}
+                    />
+                    <Route exact path="/techstack" element={<TechStack />} />
+
                     <Route path="/*" element={<Home />} />
                   </Routes>
                 </>
               ) : (
                 <>
                   <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/login" element={<Login />} />
+                    <Route path="/*" element={<Login />} />
                   </Routes>
                 </>
               )}
