3d3c2b6918a475c571dda88c8625bd5bdfa5f127
ruishanteo <ruishan.teo03@gmail.com>
2023-06-26 14:33:03 +0800

    implemented techStack store

---
+++ src/backend/techStackStore.js
@@ -0,0 +1,85 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
+
+import { db } from "./Firebase";
+import { handleApiCall } from "./FirebaseHooks";
+
+const techStackSlice = createSlice({
+  name: "techstack",
+  initialState: {
+    techStack: [],
+    tech: undefined,
+  },
+  reducers: {
+    saveTechStackToStore: (state, action) => {
+      state.techStack = action.payload;
+      state.tech = undefined;
+    },
+    saveTechToStore: (state, action) => {
+      state.tech = action.payload;
+    },
+  },
+});
+
+export async function fetchTechStack(dispatch, getState) {
+  const response = await getDocs(collection(db, "techStack"));
+  const techStack = response.docs.map((doc) => {
+    return {
+      ...doc.data(),
+      id: doc.id,
+    };
+  });
+  dispatch(techStackSlice.actions.saveTechStackToStore(techStack));
+}
+
+export function createTech(tech) {
+  return async (dispatch, getState) => {
+    return await handleApiCall(
+      setDoc(doc(db, "techStack", tech.name.toLowerCase()), {
+        name: tech.name,
+        iconImg: tech.iconImg,
+        isInProfile: tech.isInProfile,
+      })
+    );
+  };
+}
+
+export function readTech(id) {
+  return async (dispatch, getState) => {
+    const response = await getDoc(doc(db, "techStack", id));
+    if (response.exists()) {
+      dispatch(
+        techStackSlice.actions.saveTechToStore({
+          ...response.data(),
+          id: response.id,
+        })
+      );
+    } else {
+      techStackSlice.actions.saveTechToStore(undefined);
+    }
+  };
+}
+
+export function editTech(tech, id) {
+  return async (dispatch, getState) => {
+    dispatch(deleteTech(id));
+    dispatch(createTech(tech));
+    dispatch(fetchTechStack);
+  };
+}
+
+export function deleteTech(id) {
+  return async (dispatch, getState) => {
+    await handleApiCall(deleteDoc(doc(db, "techStack", id)));
+    dispatch(fetchTechStack);
+  };
+}
+
+export const techStackReducer = techStackSlice.reducer;
