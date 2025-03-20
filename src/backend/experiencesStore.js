91b1fa3ce72b9fba6daa483e584bdb5370fdef17
Rui Shan <ruishan.teo03@gmail.com>
2025-03-20 14:35:25 +0800

    Remove date

--- src/backend/experiencesStore.js
+++ src/backend/experiencesStore.js
@@ -30,7 +30,7 @@ function convertTime(date) {
   const month = months[date.getMonth()];
   const year = date.getFullYear();
   const day = date.getDate();
-  return `${day} ${month} ${year}`;
+  return `${month} ${year}`;
 }
 
 const experiencesSlice = createSlice({
